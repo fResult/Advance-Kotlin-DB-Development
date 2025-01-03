a7e9ad676fb73c60fac382cd42cb126ba0fb5c09
fResult <styxmaz@gmail.com>
2025-01-04 02:07:29 +0700

    Re-format code.

--- src/main/kotlin/01_04.kt
+++ src/main/kotlin/01_04.kt
@@ -4,21 +4,21 @@ import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
-    println("Hello Exposed!")
-    Database.connect(
-        "jdbc:postgresql://localhost:5432/sports_db",
-        user = "sports_db_admin",
-        password = "abcd1234"
-    )
+  println("Hello Exposed!")
+  Database.connect(
+    "jdbc:postgresql://localhost:5432/sports_db",
+    user = "sports_db_admin",
+    password = "abcd1234"
+  )
 
 
-    transaction {
-        SchemaUtils.drop(CustomersTable)
-        SchemaUtils.create(CustomersTable)
-    }
+  transaction {
+    SchemaUtils.drop(CustomersTable)
+    SchemaUtils.create(CustomersTable)
+  }
 }
 
 object CustomersTable : IntIdTable() {
-    val name = varchar("name", 20)
-    val email = varchar("email", 50).nullable()
+  val name = varchar("name", 20)
+  val email = varchar("email", 50).nullable()
 }
