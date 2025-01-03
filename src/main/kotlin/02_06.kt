460bb51ddc43c15beb37fd47ac888aabc97dd673
fResult <styxmaz@gmail.com>
2025-01-04 01:59:09 +0700

    Re-format code.

--- src/main/kotlin/02_06.kt
+++ src/main/kotlin/02_06.kt
@@ -1,38 +1,41 @@
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.ReferenceOption
+import org.jetbrains.exposed.sql.SortOrder
+import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
-    connect()
+  connect()
 
-    transaction {
-        CustomersTable
-            .selectAll()
-            .orderBy(CustomersTable.lastName to SortOrder.DESC)
-            .limit(10, offset = 20)
-            .forEach { row ->
-                println(row)
-            }
-    }
+  transaction {
+    CustomersTable
+      .selectAll()
+      .orderBy(CustomersTable.lastName to SortOrder.DESC)
+      .limit(10, offset = 20)
+      .forEach { row ->
+        println(row)
+      }
+  }
 }
 
 object CustomersTable : IntIdTable() {
-    val firstName = varchar("first_name", 20)
-    val lastName = varchar("last_name", 20)
-    val email = varchar("email", 50).nullable().uniqueIndex()
+  val firstName = varchar("first_name", 20)
+  val lastName = varchar("last_name", 20)
+  val email = varchar("email", 50).nullable().uniqueIndex()
 }
 
 private object OrdersTable : IntIdTable() {
-    val totalDue = varchar("total_due", 10)
-    val customerId = integer("customer_id").references(CustomersTable.id, onDelete = ReferenceOption.CASCADE)
-        .index()
+  val totalDue = varchar("total_due", 10)
+  val customerId = integer("customer_id").references(CustomersTable.id, onDelete = ReferenceOption.CASCADE)
+    .index()
 }
 
 fun connect() {
-    Database.connect(
-        "jdbc:postgresql://localhost:5432/sports_db_populated",
-        user = "sports_db_admin",
-        password = "abcd1234"
-    )
+  Database.connect(
+    "jdbc:postgresql://localhost:5432/sports_db_populated",
+    user = "sports_db_admin",
+    password = "abcd1234"
+  )
 }
 
