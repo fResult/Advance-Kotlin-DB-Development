008e10a63290ef7cbb2fcddd9416a29a2cbc6ed3
fResult <styxmaz@gmail.com>
2025-01-04 01:42:46 +0700

    Re-format code.

--- src/main/kotlin/02_02.kt
+++ src/main/kotlin/02_02.kt
@@ -1,35 +1,38 @@
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.alias
+import org.jetbrains.exposed.sql.function
+import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
-    connect()
+  connect()
 
-    transaction {
-        val capitalized = CustomersTable.firstName.function("UPPE").alias("capitalized")
-        CustomersTable
-            .slice(
-                CustomersTable.id,
-                capitalized
-            )
-            .selectAll()
-            .forEach { row ->
-                println(row[capitalized])
-            }
-    }
+  transaction {
+    val capitalized = CustomersTable.firstName.function("UPPE").alias("capitalized")
+    CustomersTable
+      .slice(
+        CustomersTable.id,
+        capitalized
+      )
+      .selectAll()
+      .forEach { row ->
+        println(row[capitalized])
+      }
+  }
 }
 
 object CustomersTable : IntIdTable() {
-    val firstName = varchar("first_name", 20)
-    val lastName = varchar("last_name", 20)
-    val email = varchar("email", 50).nullable()
+  val firstName = varchar("first_name", 20)
+  val lastName = varchar("last_name", 20)
+  val email = varchar("email", 50).nullable()
 }
 
 fun connect() {
-    Database.connect(
-        "jdbc:postgresql://localhost:5432/sports_db_populated",
-        user = "sports_db_admin",
-        password = "abcd1234"
-    )
+  Database.connect(
+    "jdbc:postgresql://localhost:5432/sports_db_populated",
+    user = "sports_db_admin",
+    password = "abcd1234"
+  )
 }
 
