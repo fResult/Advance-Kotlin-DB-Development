f34d50792af611120bd3047b86d151ee5a873431
fResult <styxmaz@gmail.com>
2025-01-04 02:35:38 +0700

    Prepare db connection.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
+import org.jetbrains.exposed.sql.Database
+
 fun main() {
   println("Hello Exposed!")
+  val db = Database.connect(
+    url = "jdbc:postgresql://localhost:5432/sports_db",
+    user = "sports_db_admin",
+    password = "12345678"
+  )
+
+  println("${db.vendor} ${db.dialect.name}")
 }
