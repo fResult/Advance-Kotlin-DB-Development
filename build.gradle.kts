4f46f682a085af0ed66a56b181cb299445f9d453
fResult <styxmaz@gmail.com>
2025-01-04 02:03:45 +0700

    Re-format code.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,24 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+  kotlin("jvm") version "1.7.10"
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.39.2")
-    implementation("org.postgresql:postgresql:42.5.0")
-    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:0.39.2")
-    testImplementation(kotlin("test"))
+  implementation("org.jetbrains.exposed:exposed-jdbc:0.39.2")
+  implementation("org.postgresql:postgresql:42.5.0")
+  implementation("org.jetbrains.exposed:exposed-kotlin-datetime:0.39.2")
+  testImplementation(kotlin("test"))
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+  kotlinOptions.jvmTarget = "11"
 }
--- src/main/kotlin/03_01.kt
+++ src/main/kotlin/03_01.kt
@@ -3,87 +3,87 @@ import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
-    connect()
+  connect()
 
-    transaction {
-        addLogger(StdOutSqlLogger)
-        val count = OrdersTable.status.count()
+  transaction {
+    addLogger(StdOutSqlLogger)
+    val count = OrdersTable.status.count()
 
-        val query = (CustomersTable innerJoin OrdersTable)
-            .slice(count, CustomersTable.city, CustomersTable.state)
-            .select { OrdersTable.status eq OrderStatus.Paid }
-            .groupBy(CustomersTable.city, CustomersTable.state)
-            .limit(10)
-            .orderBy(count to SortOrder.DESC, CustomersTable.city to SortOrder.ASC)
-            .prepareSQL(this)
+    val query = (CustomersTable innerJoin OrdersTable)
+      .slice(count, CustomersTable.city, CustomersTable.state)
+      .select { OrdersTable.status eq OrderStatus.Paid }
+      .groupBy(CustomersTable.city, CustomersTable.state)
+      .limit(10)
+      .orderBy(count to SortOrder.DESC, CustomersTable.city to SortOrder.ASC)
+      .prepareSQL(this)
 
-        println(query)
-    }
+    println(query)
+  }
 }
 
 
 object CustomersTable : IntIdTable() {
-    val firstName = varchar("first_name", 20)
-    val lastName = varchar("last_name", 20)
-    val email = varchar("email", 50).nullable().uniqueIndex()
-    val city = varchar("city", 20)
-    val state = varchar("state", 20)
+  val firstName = varchar("first_name", 20)
+  val lastName = varchar("last_name", 20)
+  val email = varchar("email", 50).nullable().uniqueIndex()
+  val city = varchar("city", 20)
+  val state = varchar("state", 20)
 }
 
 private object OrdersTable : IntIdTable() {
-    val totalDue = varchar("total_due", 10)
-    val customerId = integer("customer_id")
-        .references(CustomersTable.id, onDelete = ReferenceOption.CASCADE)
-        .index()
+  val totalDue = varchar("total_due", 10)
+  val customerId = integer("customer_id")
+    .references(CustomersTable.id, onDelete = ReferenceOption.CASCADE)
+    .index()
 
-    val status = customEnumeration(
-        "status",
-        "varchar(20)",
-        OrderStatus::fromDb,
-        OrderStatus::toDb
-    )
+  val status = customEnumeration(
+    "status",
+    "varchar(20)",
+    OrderStatus::fromDb,
+    OrderStatus::toDb
+  )
 }
 
 enum class OrderStatus {
-    Created,
-    Due,
-    PastDue,
-    Cancelled,
-    Paid,
-    Returned;
+  Created,
+  Due,
+  PastDue,
+  Cancelled,
+  Paid,
+  Returned;
 
-    companion object {
-        fun toDb(orderStatus: OrderStatus): String {
-            return when (orderStatus) {
-                Created -> "created"
-                Due -> "due"
-                PastDue -> "past due"
-                Cancelled -> "cancelled"
-                Paid -> "paid"
-                Returned -> "returned"
-            }
-        }
-
-        fun fromDb(dbValue: Any): OrderStatus {
-            return when (dbValue) {
-                "created" -> Created
-                "due" -> Due
-                "past due" -> PastDue
-                "cancelled" -> Cancelled
-                "paid" -> Paid
-                "returned" -> Returned
-                else -> throw RuntimeException("Unknown status: $dbValue")
-            }
-        }
+  companion object {
+    fun toDb(orderStatus: OrderStatus): String {
+      return when (orderStatus) {
+        Created -> "created"
+        Due -> "due"
+        PastDue -> "past due"
+        Cancelled -> "cancelled"
+        Paid -> "paid"
+        Returned -> "returned"
+      }
     }
+
+    fun fromDb(dbValue: Any): OrderStatus {
+      return when (dbValue) {
+        "created" -> Created
+        "due" -> Due
+        "past due" -> PastDue
+        "cancelled" -> Cancelled
+        "paid" -> Paid
+        "returned" -> Returned
+        else -> throw RuntimeException("Unknown status: $dbValue")
+      }
+    }
+  }
 }
 
 
 fun connect() {
-    Database.connect(
-        "jdbc:postgresql://localhost:5432/sports_db_populated",
-        user = "sports_db_admin",
-        password = "abcd1234"
-    )
+  Database.connect(
+    "jdbc:postgresql://localhost:5432/sports_db_populated",
+    user = "sports_db_admin",
+    password = "abcd1234"
+  )
 }
 
