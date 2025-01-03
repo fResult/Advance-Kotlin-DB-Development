b93c3d31cd9dfd93f9e4acb4e296753a54f92080
fResult <styxmaz@gmail.com>
2025-01-04 01:59:39 +0700

    Re-format code.

--- src/main/kotlin/02_07.kt
+++ src/main/kotlin/02_07.kt
@@ -3,48 +3,48 @@ import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
-    connect()
+  connect()
 
-    transaction {
-        SchemaUtils.drop(OrdersTable)
-        SchemaUtils.create(OrdersTable)
-        OrdersTable.insert { row ->
-            row[totalDue] = "370.86"
-            row[customerId] = 789
-            row[status] = OrderStatus.Created
-        }
+  transaction {
+    SchemaUtils.drop(OrdersTable)
+    SchemaUtils.create(OrdersTable)
+    OrdersTable.insert { row ->
+      row[totalDue] = "370.86"
+      row[customerId] = 789
+      row[status] = OrderStatus.Created
     }
+  }
 }
 
 object CustomersTable : IntIdTable() {
-    val firstName = varchar("first_name", 20)
-    val lastName = varchar("last_name", 20)
-    val email = varchar("email", 50).nullable().uniqueIndex()
+  val firstName = varchar("first_name", 20)
+  val lastName = varchar("last_name", 20)
+  val email = varchar("email", 50).nullable().uniqueIndex()
 }
 
 private object OrdersTable : IntIdTable() {
-    val totalDue = varchar("total_due", 10)
-    val customerId = integer("customer_id").references(CustomersTable.id, onDelete = ReferenceOption.CASCADE)
-        .index()
+  val totalDue = varchar("total_due", 10)
+  val customerId = integer("customer_id").references(CustomersTable.id, onDelete = ReferenceOption.CASCADE)
+    .index()
 
-    val status = enumerationByName<OrderStatus>("status", 20)
+  val status = enumerationByName<OrderStatus>("status", 20)
 }
 
 enum class OrderStatus {
-    Created,
-    Due,
-    PastDue,
-    Cancelled,
-    Paid,
-    Returned,
+  Created,
+  Due,
+  PastDue,
+  Cancelled,
+  Paid,
+  Returned,
 }
 
 
 fun connect() {
-    Database.connect(
-        "jdbc:postgresql://localhost:5432/sports_db_populated",
-        user = "sports_db_admin",
-        password = "abcd1234"
-    )
+  Database.connect(
+    "jdbc:postgresql://localhost:5432/sports_db_populated",
+    user = "sports_db_admin",
+    password = "abcd1234"
+  )
 }
 
