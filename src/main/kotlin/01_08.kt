7c249426c7f8f40f67ae8b5ff76fb546d9dea8a6
fResult <styxmaz@gmail.com>
2025-01-04 02:07:55 +0700

    Re-format code.

--- src/main/kotlin/01_08.kt
+++ src/main/kotlin/01_08.kt
@@ -1,55 +1,55 @@
-import CustomersTable.email
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
-    println("Hello Exposed!")
-    connect()
+  println("Hello Exposed!")
+  connect()
 
-    transaction {
-        recreateTables()
-        createCustomers()
+  transaction {
+    recreateTables()
+    createCustomers()
 
-        CustomersTable.update({ CustomersTable.id eq 2}){ row ->
-            row[email] = "ecarr1@oracle.com"
-        }
-
-        CustomersTable.update({ CustomersTable.name eq "Carol"}){ row ->
-            with(SqlExpressionBuilder) {
-                row[name] = concat(name, name)
-            }
-        }
+    CustomersTable.update({ CustomersTable.id eq 2 }) { row ->
+      row[email] = "ecarr1@oracle.com"
     }
+
+    CustomersTable.update({ CustomersTable.name eq "Carol" }) { row ->
+      with(SqlExpressionBuilder) {
+        row[name] = concat(name, name)
+      }
+    }
+  }
 }
 
 fun createCustomers() = transaction {
-    CustomersTable.insert { row ->
-        row[name] = "Carol"
-    }
+  CustomersTable.insert { row ->
+    row[name] = "Carol"
+  }
 
-    val newId = CustomersTable.insertAndGetId { row ->
-        row[name] = "Elisabeth"
-    }
+  val newId = CustomersTable.insertAndGetId { row ->
+    row[name] = "Elisabeth"
+  }
 
-    val newRow = CustomersTable.insert { row ->
-        row[name] = "Ernest"
-    }.resultedValues?.first()
+  val newRow = CustomersTable.insert { row ->
+    row[name] = "Ernest"
+  }.resultedValues?.first()
 }
 
 fun recreateTables() = transaction {
-    SchemaUtils.drop(CustomersTable)
-    SchemaUtils.create(CustomersTable)
+  SchemaUtils.drop(CustomersTable)
+  SchemaUtils.create(CustomersTable)
 }
+
 fun connect() {
-    Database.connect(
-        "jdbc:postgresql://localhost:5432/sports_db",
-        user = "sports_db_admin",
-        password = "abcd1234"
-    )
+  Database.connect(
+    "jdbc:postgresql://localhost:5432/sports_db",
+    user = "sports_db_admin",
+    password = "abcd1234"
+  )
 }
 
 object CustomersTable : IntIdTable() {
-    val name = varchar("name", 20)
-    val email = varchar("email", 50).nullable()
+  val name = varchar("name", 20)
+  val email = varchar("email", 50).nullable()
 }
