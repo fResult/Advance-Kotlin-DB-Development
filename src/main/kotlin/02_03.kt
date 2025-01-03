5fa25edfb232d509f438f2049e1b56335c614ab3
fResult <styxmaz@gmail.com>
2025-01-04 01:55:33 +0700

    Re-format code.

--- src/main/kotlin/02_03.kt
+++ src/main/kotlin/02_03.kt
@@ -1,34 +1,36 @@
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.ReferenceOption
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.transactions.transaction
-import java.math.BigDecimal
 
 fun main() {
-    connect()
+  connect()
 
-    transaction {
-        SchemaUtils.createMissingTablesAndColumns(OrdersTable)
+  transaction {
+    SchemaUtils.createMissingTablesAndColumns(OrdersTable)
 
-        CustomersTable.deleteWhere { CustomersTable.id eq 100 }
-    }
+    CustomersTable.deleteWhere { CustomersTable.id eq 100 }
+  }
 }
 
 object CustomersTable : IntIdTable() {
-    val firstName = varchar("first_name", 20)
-    val lastName = varchar("last_name", 20)
-    val email = varchar("email", 50).nullable()
+  val firstName = varchar("first_name", 20)
+  val lastName = varchar("last_name", 20)
+  val email = varchar("email", 50).nullable()
 }
 
 private object OrdersTable : IntIdTable() {
-    val totalDue = varchar("total_due", 10)
-    val customerId = integer("customer_id").references(CustomersTable.id, onDelete = ReferenceOption.CASCADE)
+  val totalDue = varchar("total_due", 10)
+  val customerId = integer("customer_id").references(CustomersTable.id, onDelete = ReferenceOption.CASCADE)
 }
 
 fun connect() {
-    Database.connect(
-        "jdbc:postgresql://localhost:5432/sports_db_populated",
-        user = "sports_db_admin",
-        password = "abcd1234"
-    )
+  Database.connect(
+    "jdbc:postgresql://localhost:5432/sports_db_populated",
+    user = "sports_db_admin",
+    password = "abcd1234"
+  )
 }
 
