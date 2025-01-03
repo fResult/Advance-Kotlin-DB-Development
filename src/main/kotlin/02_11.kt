04addf00584cd8cecf1b2aa630373365e29a2ef0
fResult <styxmaz@gmail.com>
2025-01-04 02:01:33 +0700

    Re-format code.

--- src/main/kotlin/02_11.kt
+++ src/main/kotlin/02_11.kt
@@ -1,81 +1,83 @@
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.ReferenceOption
+import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
-    connect()
+  connect()
 
-    transaction {
-        OrdersTable.select {
-            OrdersTable.status eq OrderStatus.PastDue
-        }.forEach {
-            println(it)
-        }
+  transaction {
+    OrdersTable.select {
+      OrdersTable.status eq OrderStatus.PastDue
+    }.forEach {
+      println(it)
     }
+  }
 }
 
 
 object CustomersTable : IntIdTable() {
-    val firstName = varchar("first_name", 20)
-    val lastName = varchar("last_name", 20)
-    val email = varchar("email", 50).nullable().uniqueIndex()
-    val city = varchar("city", 20)
+  val firstName = varchar("first_name", 20)
+  val lastName = varchar("last_name", 20)
+  val email = varchar("email", 50).nullable().uniqueIndex()
+  val city = varchar("city", 20)
 }
 
 private object OrdersTable : IntIdTable() {
-    val totalDue = varchar("total_due", 10)
-    val customerId = integer("customer_id")
-        .references(CustomersTable.id, onDelete = ReferenceOption.CASCADE)
-        .index()
+  val totalDue = varchar("total_due", 10)
+  val customerId = integer("customer_id")
+    .references(CustomersTable.id, onDelete = ReferenceOption.CASCADE)
+    .index()
 
-    val status = customEnumeration(
-        "status",
-        "varchar(20)",
-        OrderStatus::fromDb,
-        OrderStatus::toDb
-    )
+  val status = customEnumeration(
+    "status",
+    "varchar(20)",
+    OrderStatus::fromDb,
+    OrderStatus::toDb
+  )
 }
 
 enum class OrderStatus {
-    Created,
-    Due,
-    PastDue,
-    Cancelled,
-    Paid,
-    Returned;
+  Created,
+  Due,
+  PastDue,
+  Cancelled,
+  Paid,
+  Returned;
 
-    companion object {
-        fun toDb(orderStatus: OrderStatus): String {
-            return when (orderStatus) {
-                Created -> "created"
-                Due -> "due"
-                PastDue -> "past due"
-                Cancelled -> "cancelled"
-                Paid -> "paid"
-                Returned -> "returned"
-            }
-        }
-
-        fun fromDb(dbValue: Any): OrderStatus {
-            return when (dbValue) {
-                "created" -> Created
-                "due" -> Due
-                "past due" -> PastDue
-                "cancelled" -> Cancelled
-                "paid" -> Paid
-                "returned" -> Returned
-                else -> throw RuntimeException("Unknown status: $dbValue")
-            }
-        }
+  companion object {
+    fun toDb(orderStatus: OrderStatus): String {
+      return when (orderStatus) {
+        Created -> "created"
+        Due -> "due"
+        PastDue -> "past due"
+        Cancelled -> "cancelled"
+        Paid -> "paid"
+        Returned -> "returned"
+      }
     }
+
+    fun fromDb(dbValue: Any): OrderStatus {
+      return when (dbValue) {
+        "created" -> Created
+        "due" -> Due
+        "past due" -> PastDue
+        "cancelled" -> Cancelled
+        "paid" -> Paid
+        "returned" -> Returned
+        else -> throw RuntimeException("Unknown status: $dbValue")
+      }
+    }
+  }
 }
 
 
 fun connect() {
-    Database.connect(
-        "jdbc:postgresql://localhost:5432/sports_db_populated",
-        user = "sports_db_admin",
-        password = "abcd1234"
-    )
+  Database.connect(
+    "jdbc:postgresql://localhost:5432/sports_db_populated",
+    user = "sports_db_admin",
+    password = "abcd1234"
+  )
 }
 
