57c3dd3af473ca106ae869ae63c06ab7b223feee
fResult <styxmaz@gmail.com>
2025-01-04 02:00:36 +0700

    Re-format code.

--- src/main/kotlin/02_09.kt
+++ src/main/kotlin/02_09.kt
@@ -1,51 +1,52 @@
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.*
-import org.jetbrains.exposed.sql.kotlin.datetime.CurrentDateTime
-import org.jetbrains.exposed.sql.kotlin.datetime.datetime
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.ReferenceOption
+import org.jetbrains.exposed.sql.SortOrder
+import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
-    connect()
+  connect()
 
-    transaction {
-        (OrdersTable innerJoin CustomersTable)
-            .slice(OrdersTable.totalDue, CustomersTable.firstName, CustomersTable.lastName)
-            .selectAll()
-            .limit(10)
-            .orderBy(
-                CustomersTable.firstName to SortOrder.ASC,
-                CustomersTable.lastName to SortOrder.ASC
-            )
-            .forEach {
-                println(it)
-            }
+  transaction {
+    (OrdersTable innerJoin CustomersTable)
+      .slice(OrdersTable.totalDue, CustomersTable.firstName, CustomersTable.lastName)
+      .selectAll()
+      .limit(10)
+      .orderBy(
+        CustomersTable.firstName to SortOrder.ASC,
+        CustomersTable.lastName to SortOrder.ASC
+      )
+      .forEach {
+        println(it)
+      }
 
-        (CustomersTable leftJoin OrdersTable)
-            .selectAll()
-            .forEach {
-                println(it)
-            }
-    }
+    (CustomersTable leftJoin OrdersTable)
+      .selectAll()
+      .forEach {
+        println(it)
+      }
+  }
 }
 
 object CustomersTable : IntIdTable() {
-    val firstName = varchar("first_name", 20)
-    val lastName = varchar("last_name", 20)
-    val email = varchar("email", 50).nullable().uniqueIndex()
+  val firstName = varchar("first_name", 20)
+  val lastName = varchar("last_name", 20)
+  val email = varchar("email", 50).nullable().uniqueIndex()
 }
 
 private object OrdersTable : IntIdTable() {
-    val totalDue = varchar("total_due", 10)
-    val customerId = integer("customer_id")
-        .references(CustomersTable.id, onDelete = ReferenceOption.CASCADE)
-        .index()
+  val totalDue = varchar("total_due", 10)
+  val customerId = integer("customer_id")
+    .references(CustomersTable.id, onDelete = ReferenceOption.CASCADE)
+    .index()
 }
 
 fun connect() {
-    Database.connect(
-        "jdbc:postgresql://localhost:5432/sports_db_populated",
-        user = "sports_db_admin",
-        password = "abcd1234"
-    )
+  Database.connect(
+    "jdbc:postgresql://localhost:5432/sports_db_populated",
+    user = "sports_db_admin",
+    password = "abcd1234"
+  )
 }
 
