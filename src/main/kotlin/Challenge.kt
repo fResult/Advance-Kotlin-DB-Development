8c031ff54c929b72de38db14d2d5575d543b3519
fResult <styxmaz@gmail.com>
2025-01-04 02:08:51 +0700

    Re-format code.

--- src/main/kotlin/Challenge.kt
+++ src/main/kotlin/Challenge.kt
@@ -1,29 +1,30 @@
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
-    connect()
+  connect()
 
-    transaction {
-        recreateTables()
-    }
+  transaction {
+    recreateTables()
+  }
 }
 
 fun recreateTables() = transaction {
-    SchemaUtils.drop(CustomersTable)
-    SchemaUtils.create(CustomersTable)
+  SchemaUtils.drop(CustomersTable)
+  SchemaUtils.create(CustomersTable)
 }
 
 fun connect() {
-    Database.connect(
-        "jdbc:postgresql://localhost:5432/sports_db",
-        user = "sports_db_admin",
-        password = "abcd1234"
-    )
+  Database.connect(
+    "jdbc:postgresql://localhost:5432/sports_db",
+    user = "sports_db_admin",
+    password = "abcd1234"
+  )
 }
 
 object CustomersTable : IntIdTable() {
-    val name = varchar("name", 20)
-    val email = varchar("email", 50).nullable()
+  val name = varchar("name", 20)
+  val email = varchar("email", 50).nullable()
 }
