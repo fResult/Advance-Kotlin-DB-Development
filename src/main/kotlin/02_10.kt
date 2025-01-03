56011dac1f1db1a3b25b240d7905d84d20dba6e5
fResult <styxmaz@gmail.com>
2025-01-04 02:01:01 +0700

    Re-format code.

--- src/main/kotlin/02_10.kt
+++ src/main/kotlin/02_10.kt
@@ -1,41 +1,44 @@
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.ReferenceOption
+import org.jetbrains.exposed.sql.count
+import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
-    connect()
+  connect()
 
-    transaction {
-        val count = CustomersTable.city.count()
-        CustomersTable.slice(count, CustomersTable.city)
-            .selectAll()
-            .groupBy(CustomersTable.city)
-            .having { count greater 10 }
-            .forEach {
-                println(it)
-            }
-    }
+  transaction {
+    val count = CustomersTable.city.count()
+    CustomersTable.slice(count, CustomersTable.city)
+      .selectAll()
+      .groupBy(CustomersTable.city)
+      .having { count greater 10 }
+      .forEach {
+        println(it)
+      }
+  }
 }
 
 object CustomersTable : IntIdTable() {
-    val firstName = varchar("first_name", 20)
-    val lastName = varchar("last_name", 20)
-    val email = varchar("email", 50).nullable().uniqueIndex()
-    val city = varchar("city", 20)
+  val firstName = varchar("first_name", 20)
+  val lastName = varchar("last_name", 20)
+  val email = varchar("email", 50).nullable().uniqueIndex()
+  val city = varchar("city", 20)
 }
 
 private object OrdersTable : IntIdTable() {
-    val totalDue = varchar("total_due", 10)
-    val customerId = integer("customer_id")
-        .references(CustomersTable.id, onDelete = ReferenceOption.CASCADE)
-        .index()
+  val totalDue = varchar("total_due", 10)
+  val customerId = integer("customer_id")
+    .references(CustomersTable.id, onDelete = ReferenceOption.CASCADE)
+    .index()
 }
 
 fun connect() {
-    Database.connect(
-        "jdbc:postgresql://localhost:5432/sports_db_populated",
-        user = "sports_db_admin",
-        password = "abcd1234"
-    )
+  Database.connect(
+    "jdbc:postgresql://localhost:5432/sports_db_populated",
+    user = "sports_db_admin",
+    password = "abcd1234"
+  )
 }
 
