5f2b637fdddd46767a7a97650774b4c485ec0eeb
fResult <styxmaz@gmail.com>
2025-01-04 02:06:58 +0700

    Re-format code.

--- src/main/kotlin/01_02.kt
+++ src/main/kotlin/01_02.kt
@@ -2,14 +2,14 @@ import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun main() {
-    println("Hello Exposed!")
-    Database.connect(
-        "jdbc:postgresql://localhost:5432/sports_db",
-        user = "sports_db_admin",
-        password = "abcd1234"
-    )
+  println("Hello Exposed!")
+  Database.connect(
+    "jdbc:postgresql://localhost:5432/sports_db",
+    user = "sports_db_admin",
+    password = "abcd1234"
+  )
 
-    transaction {
+  transaction {
 
-    }
+  }
 }
