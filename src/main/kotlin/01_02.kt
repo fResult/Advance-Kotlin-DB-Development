1e3428628c310f8a190b964d2b82397559eb237e
fResult <styxmaz@gmail.com>
2025-01-05 03:50:00 +0700

    Update build.gradle.kts, and postgresql's port.

--- src/main/kotlin/01_02.kt
+++ src/main/kotlin/01_02.kt
@@ -4,7 +4,7 @@ import org.jetbrains.exposed.sql.transactions.transaction
 fun main() {
   println("Hello Exposed!")
   val db = Database.connect(
-    "jdbc:postgresql://localhost:5432/sports_db",
+    "jdbc:postgresql://localhost:5431/sports_db",
     user = "sports_db_admin",
     password = "12345678"
   )
