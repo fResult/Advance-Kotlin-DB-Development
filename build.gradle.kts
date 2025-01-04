2e6c100ad1e350a1a22923a57738399c1464d4e8
fResult <styxmaz@gmail.com>
2025-01-05 03:48:06 +0700

    Update build.gradle.kts, and postgresql's port.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   kotlin("jvm") version "1.7.10"
+  application
 }
 
 repositories {
@@ -20,4 +21,8 @@ tasks.test {
 
 tasks.withType<KotlinCompile> {
   kotlinOptions.jvmTarget = "11"
+}
+
+application {
+  mainClass.set("MainKt")
 }
--- src/main/kotlin/01_01.kt
+++ src/main/kotlin/01_01.kt
@@ -3,8 +3,8 @@ import org.jetbrains.exposed.sql.Database
 fun main() {
   println("Hello Exposed!")
   Database.connect(
-    "jdbc:postgresql://localhost:5432/sports_db",
+    "jdbc:postgresql://localhost:5431/sports_db",
     user = "sports_db_admin",
-    password = "abcd1234"
+    password = "12345678"
   )
 }
