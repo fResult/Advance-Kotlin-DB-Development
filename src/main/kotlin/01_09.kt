24e198a60421a049f5dea6dd8a5dd8064764d420
fResult <styxmaz@gmail.com>
2025-01-05 03:54:20 +0700

    Update build.gradle.kts, and postgresql's port.

--- src/main/kotlin/01_09.kt
+++ src/main/kotlin/01_09.kt
@@ -37,9 +37,9 @@ fun recreateTables() = transaction {
 
 fun connect() {
   Database.connect(
-    "jdbc:postgresql://localhost:5432/sports_db",
+    "jdbc:postgresql://localhost:5431/sports_db",
     user = "sports_db_admin",
-    password = "abcd1234"
+    password = "12345678"
   )
 }
 
