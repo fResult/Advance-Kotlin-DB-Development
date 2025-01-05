70006a95bff35a61c1bdc4b8e045264358460f21
fResult <styxmaz@gmail.com>
2025-01-06 02:56:21 +0700

    Prepare \`OrderStatus.toDb\` and \`OrderStatus.fromdb\` enum methods.

--- src/main/kotlin/enumerations/OrderStatus.kt
+++ src/main/kotlin/enumerations/OrderStatus.kt
@@ -1,11 +1,35 @@
 package enumerations
 
 enum class OrderStatus {
-  Pending,
   Created,
   Due,
   PastDue,
   Cancelled,
   Paid,
-  Returned,
+  Returned;
+
+  companion object {
+    fun toDb(orderStatus: OrderStatus): String {
+      return when (orderStatus) {
+        Created -> "created"
+        Due -> "due"
+        PastDue -> "past due"
+        Cancelled -> "cancelled"
+        Paid -> "paid"
+        Returned -> "returned"
+      }
+    }
+
+    fun fromDb(dbValue: Any): OrderStatus {
+      return when (dbValue) {
+        "created" -> Created
+        "due" -> Due
+        "past due" -> PastDue
+        "cancelled" -> Cancelled
+        "paid" -> Paid
+        "returned" -> Returned
+        else -> throw RuntimeException("Unknown status: $dbValue")
+      }
+    }
+  }
 }
