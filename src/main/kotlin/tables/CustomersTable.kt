d8b2da66f77bd9be04e191138e07f0bc0b8f540c
fResult <styxmaz@gmail.com>
2025-01-06 01:32:37 +0700

    Add \`customers.city\` column.

--- src/main/kotlin/tables/CustomersTable.kt
+++ src/main/kotlin/tables/CustomersTable.kt
@@ -1,11 +1,14 @@
 package tables
 
-import org.jetbrains.exposed.dao.id.LongIdTable
+import org.jetbrains.exposed.sql.Table.Dual.nullable
+import org.jetbrains.exposed.sql.Table.Dual.uniqueIndex
+import org.jetbrains.exposed.sql.Table.Dual.varchar
 
-object CustomersTable : LongIdTable("customers") {
-  val firstName = varchar("first_name", 20)
+object CustomersTable {
+ val firstName = varchar("first_name", 20)
   val lastName = varchar("last_name", 30)
   val email = varchar("email", 50)
     .nullable()
     .uniqueIndex()
+  val city = varchar("city", 20)
 }
