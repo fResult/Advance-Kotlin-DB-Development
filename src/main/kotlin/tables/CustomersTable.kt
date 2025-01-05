851514b5448a97f80b84d90bb8ef9d2dd1af0a9b
fResult <styxmaz@gmail.com>
2025-01-05 17:21:58 +0700

    Alter \`customers.email\` column to be unique column.

--- src/main/kotlin/tables/CustomersTable.kt
+++ src/main/kotlin/tables/CustomersTable.kt
@@ -5,5 +5,7 @@ import org.jetbrains.exposed.dao.id.LongIdTable
 object CustomersTable : LongIdTable("customers") {
   val firstName = varchar("first_name", 20)
   val lastName = varchar("last_name", 30)
-  val email = varchar("email", 50).nullable()
+  val email = varchar("email", 50)
+    .nullable()
+    .uniqueIndex()
 }
