8b02c31ed4abb0330646c99594bd8e21bb878c71
fResult <styxmaz@gmail.com>
2025-01-06 13:23:43 +0700

    Prepare for lesson 03 unit 01.

--- src/main/kotlin/lessons/Lesson03.kt
+++ src/main/kotlin/lessons/Lesson03.kt
@@ -1,9 +1,31 @@
 package lessons
 
+import enumerations.OrderStatus
+import org.jetbrains.exposed.sql.SortOrder
+import org.jetbrains.exposed.sql.alias
+import org.jetbrains.exposed.sql.count
 import org.jetbrains.exposed.sql.transactions.transaction
+import tables.CustomersTable
+import tables.OrdersTable
 
 fun lesson03() {
   transaction {
-    do0212Challenge()
+    val city = CustomersTable.city
+    val cityCount = city.count()
+
+    (OrdersTable innerJoin CustomersTable)
+      .select(
+        cityCount.alias("paid_order_count"),
+        city,
+        CustomersTable.state,
+      )
+      .where { OrdersTable.status eq OrderStatus.Paid }
+      .limit(10)
+      .groupBy(city, CustomersTable.state)
+      .orderBy(
+        cityCount to SortOrder.DESC,
+        city to SortOrder.ASC
+      )
+      .forEach(::println)
   }
 }
