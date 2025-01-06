d1116b7acf5fbc5f07a1e6f3e502b67c10fb88c4
fResult <styxmaz@gmail.com>
2025-01-07 02:46:51 +0700

    Try customers selection unit test (03_03).

---
+++ src/test/kotlin/Lesson03.kt
@@ -0,0 +1,55 @@
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.insertAndGetId
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import tables.CustomersTable
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+class CustomerTableTest {
+  private lateinit var db: Database
+
+  @BeforeEach
+  fun setUp() {
+    db = Database.connect(
+      "jdbc:postgresql://localhost:5431/sports_db_test",
+      user = "sports_db_admin",
+      password = "12345678",
+    )
+
+    transaction { SchemaUtils.create(CustomersTable) }
+  }
+
+  @AfterEach
+  fun cleanUp() {
+    transaction { SchemaUtils.drop(CustomersTable) }
+
+    db.connector().close()
+  }
+
+  @Test
+  fun `insert creates a new row in Customers`(): Unit = transaction {
+    val newId = CustomersTable.insertAndGetId { row ->
+      row[firstName] = "John"
+      row[lastName] = "Wick"
+      row[city] = "New York"
+      row[state] = "NY"
+    }
+
+    val query = CustomersTable.selectAll()
+      .where(CustomersTable.id eq newId)
+
+    val statement = query.prepareSQL(this)
+    println(statement)
+
+    val row = query.firstOrNull()
+
+    assertNotNull(row)
+    assertEquals("John", row[CustomersTable.firstName])
+  }
+}
